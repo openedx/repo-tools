919ebe3696b3beaf5eac9d48df866c6079d99637
Ned Batchelder <ned@nedbatchelder.com>
2014-05-14 13:15:56 -0400

    Move paginated_get to a place others can use

--- author_check.py
+++ author_check.py
@@ -29,6 +29,8 @@ import github3
 import requests
 import yaml
 
+from helpers import paginated_get
+
 
 # Global data.
 
@@ -61,28 +63,6 @@ print_red = functools.partial(print_in_color, colors.red)
 print_yellow = functools.partial(print_in_color, colors.yellow)
 print_green = functools.partial(print_in_color, colors.green)
 
-
-
-def paginated_get(url, debug=False, **kwargs):
-    """
-    Returns a generator that will retrieve all objects from a paginated API.
-    Assumes that the pagination is specified in the "link" header, like
-    Github's v3 API.
-    """
-    while url:
-        resp = requests.get(url, **kwargs)
-        result = resp.json()
-        if not resp.ok:
-            raise requests.exceptions.RequestException(result["message"])
-        if debug:
-            pprint.pprint(result, stream=sys.stderr)
-        for item in result:
-            yield item
-        url = None
-        if "link" in resp.headers:
-            match = re.search(r'<(?P<url>[^>]+)>; rel="next"', resp.headers["link"])
-            if match:
-                url = match.group('url')
 
 
 # URL patterns
---
+++ helpers.py
@@ -0,0 +1,28 @@
+"""Helpers for various things."""
+
+import pprint
+import re
+
+import requests
+
+
+def paginated_get(url, debug=False, **kwargs):
+    """
+    Returns a generator that will retrieve all objects from a paginated API.
+    Assumes that the pagination is specified in the "link" header, like
+    Github's v3 API.
+    """
+    while url:
+        resp = requests.get(url, **kwargs)
+        result = resp.json()
+        if not resp.ok:
+            raise requests.exceptions.RequestException(result["message"])
+        if debug:
+            pprint.pprint(result, stream=sys.stderr)
+        for item in result:
+            yield item
+        url = None
+        if "link" in resp.headers:
+            match = re.search(r'<(?P<url>[^>]+)>; rel="next"', resp.headers["link"])
+            if match:
+                url = match.group('url')
