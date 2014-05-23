b43e9048845f1931e4a1dd0efc5ae50f1c215b37
Ned Batchelder <ned@nedbatchelder.com>
2014-05-23 13:17:22 -0400

    Move the wall code into its own runnable script

--- show-pulls.py
+++ show-pulls.py
@@ -91,30 +91,6 @@ if 0:
         for ym, data in sorted(months.items()):
             print("{ym},{data[opened]},{data[merged]}".format(ym=ym, data=data))
 
-if 0:
-    # The wall of shame
-    def show_pulls(jrep, labels=None, show_comments=False, state="open", since=None, org=False):
-        issues = get_pulls(state="open")
-        blocked_by = collections.defaultdict(list)
-        for issue in issues:
-            issue.finish_loading()
-            for label in issue['labels']:
-                if label == "osc":
-                    continue
-                blocked_by[label].append(issue)
-
-        shame = sorted(blocked_by.items(), key=lambda li: len(li[1]), reverse=True)
-        print("team,external,internal,extlines,intlines")
-        for label, issues in shame:
-            internal, external = [0, 0], [0, 0]
-            for iss in issues:
-                lines = iss['pull.additions'] + iss['pull.deletions']
-                stats = external if "osc" in iss['labels'] else internal
-                stats[0] += 1
-                stats[1] += lines
-            print("{}\t{}\t{}\t{}\t{}".format(
-                label, external[0], internal[0], external[1], internal[1]
-            ))
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Summarize pull requests.")
---
+++ wall.py
@@ -0,0 +1,33 @@
+import collections
+
+import jreport
+from pulls import get_pulls
+
+
+def show_wall(jrep):
+    issues = get_pulls(state="open")
+    blocked_by = collections.defaultdict(list)
+    for issue in issues:
+        issue.finish_loading()
+        for label in issue['labels']:
+            if label == "osc":
+                continue
+            blocked_by[label].append(issue)
+
+    shame = sorted(blocked_by.items(), key=lambda li: len(li[1]), reverse=True)
+    print("team,external,internal,extlines,intlines")
+    for label, issues in shame:
+        internal, external = [0, 0], [0, 0]
+        for iss in issues:
+            lines = iss['pull.additions'] + iss['pull.deletions']
+            stats = external if "osc" in iss['labels'] else internal
+            stats[0] += 1
+            stats[1] += lines
+        print("{}\t{}\t{}\t{}\t{}".format(
+            label, external[0], internal[0], external[1], internal[1]
+        ))
+
+
+if __name__ == "__main__":
+    jrep = jreport.JReport()
+    show_wall(jrep)
