d623e3fdbc2fe75bd9bc7299eb0242be8149ac2f
Ned Batchelder <ned@nedbatchelder.com>
2014-05-26 09:00:19 -0400

    Bucket names come from .py

--- wall.py
+++ wall.py
@@ -87,26 +87,13 @@ def show_wall():
     for team, data in blocked_by.iteritems():
         data["team"] = team
 
-    #print(json.dumps(dict(blocked_by), indent=4))
-    shame = sorted(blocked_by.values(), key=lambda d: d["total"], reverse=True)
+    teams = sorted(blocked_by.values(), key=lambda d: d["total"], reverse=True)
 
-    print(json.dumps(shame, indent=4))
-
-    return
-
-    shame = sorted(blocked_by.items(), key=lambda li: len(li[1]), reverse=True)
-    print("team,external,internal,extlines,intlines")
-    for label, issues in shame:
-        internal, external = [0, 0], [0, 0]
-        for iss in issues:
-            lines = iss['pull.additions'] + iss['pull.deletions']
-            stats = external if "osc" in iss['labels'] else internal
-            stats[0] += 1
-            stats[1] += lines
-        print("{}\t{}\t{}\t{}\t{}".format(
-            label, external[0], internal[0], external[1], internal[1]
-        ))
-
+    all_data = {
+        "buckets": [ab[1] for ab in reversed(age_buckets)],
+        "teams": teams,
+    }
+    print(json.dumps(all_data, indent=4))
 
 if __name__ == "__main__":
     show_wall()
