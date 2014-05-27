73ce79c657bd85166add911484b7008329cdf791
Ned Batchelder <ned@nedbatchelder.com>
2014-05-27 12:11:01 -0400

    A tool to find unchecked repos, and add a few repos to repos.yaml

---
+++ unchecked-repos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""List repos missing from repos.yaml."""
+
+from __future__ import print_function
+
+import json
+
+import requests
+import yaml
+
+from helpers import paginated_get
+
+REPOS_URL = "https://api.github.com/orgs/{org}/repos"
+
+with open("repos.yaml") as repos_yaml:
+    tracked_repos = yaml.load(repos_yaml)
+
+for r in paginated_get(REPOS_URL.format(org="edX")):
+    if not r['private'] and not r['fork']:
+        if r['full_name'] not in tracked_repos:
+            print("{r[full_name]}: {r[description]}".format(r=r))
