45b4da94aea122ad66b722598db88cbd8630f461
Ned Batchelder <ned@nedbatchelder.com>
2014-06-02 09:35:51 -0400

    Try to stop empty files being written on Heroku

--- dash.py
+++ dash.py
@@ -1,3 +1,6 @@
+import binascii
+from contextlib import contextmanager
+import os
 import time
 
 from age.age import get_wall_data
@@ -20,11 +23,25 @@ def boom():
 def write_age():
     start = time.time()
     age_json = get_wall_data()
-    end = time.time()
-    with open("age/age.json", "w") as age_json_file:
+    with replace_file("age/age.json") as age_json_file:
         age_json_file.write(age_json)
-        return "Wrote {} bytes in {:.1f}s".format(len(age_json), end - start)
+    end = time.time()
+    return "Wrote {} bytes in {:.1f}s".format(len(age_json), end - start)
 
 @app.route('/age/<path:filename>')
 def send_static(filename):
     return send_from_directory('age', filename)
+
+
+@contextmanager
+def replace_file(name):
+    tmpname = '%s.tmp-%s' % (name, binascii.hexlify(os.urandom(10)))
+    try:
+        with open(tmpname, 'w+') as f:
+            yield f
+        os.rename(tmpname, name)
+    finally:
+        try:
+            os.unlink(tmpname)
+        except OSError:
+            pass
