4690278f38cfad2a89c004c2bcdcdc05a0bf7f67
Ned Batchelder <ned@nedbatchelder.com>
2014-07-09 13:26:42 -0400

    Better pull-quarters

--- pull-quarters.py
+++ pull-quarters.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
 """
 Calculate pull requests opened and merged, by quarter.
 """
 
 from __future__ import print_function
 
+import argparse
 import collections
 import datetime
 import pprint
+import sys
 
 import dateutil.parser
 
@@ -14,18 +17,18 @@ from pulls import get_pulls
 from repos import Repo
 
 
-if 1:
-    def date_bucket(date):
-        """Compute the quarter for a date."""
-        date += datetime.timedelta(days=180)    # to almost get to our fiscal year
-        m = (date.month-1) // 3 + 1
-        return "Y{:02d} Q{}".format(date.year % 100, m)
-else:
-    def date_bucket(date):
-        """Compute the year and month for a date."""
-        return "Y{:02d} M{:02d}".format(date.year % 100, date.month)
+def date_bucket_quarter(date):
+    """Compute the quarter for a date."""
+    date += datetime.timedelta(days=180)    # to almost get to our fiscal year
+    m = (date.month-1) // 3 + 1
+    return "Y{:02d} Q{}".format(date.year % 100, m)
 
-def get_all_repos():
+def date_bucket_month(date):
+    """Compute the year and month for a date."""
+    return "Y{:02d} M{:02d}".format(date.year % 100, date.month)
+
+
+def get_all_repos(date_bucket_fn):
     repos = [ r for r in Repo.from_yaml() if r.track_pulls ]
 
     def bucket_blank():
@@ -42,7 +45,7 @@ def get_all_repos():
 
     buckets = collections.defaultdict(bucket_blank)
     for repo in repos:
-        get_bucket_data(buckets, repo.name)
+        get_bucket_data(buckets, repo.name, date_bucket_fn)
 
     print("qrtr\topened internal\tmerged internal\topened external\tmerged external")
     for q in sorted(buckets.keys()):
@@ -54,13 +57,29 @@ def get_all_repos():
             data["merged"]["external"],
         ))
 
-def get_bucket_data(buckets, repo_name):
+def get_bucket_data(buckets, repo_name, date_bucket_fn):
     for pull in get_pulls(repo_name, state="all", pull_details="list", org=True):
         # print("{0[id]}: {0[combinedstate]} {0[intext]}".format(pull))
         created = dateutil.parser.parse(pull['created_at'])
-        buckets[date_bucket(created)]["opened"][pull["intext"]] += 1
+        buckets[date_bucket_fn(created)]["opened"][pull["intext"]] += 1
         if pull['combinedstate'] == "merged":
             merged = dateutil.parser.parse(pull['pull.merged_at'])
-            buckets[date_bucket(merged)]["merged"][pull["intext"]] += 1
+            buckets[date_bucket_fn(merged)]["merged"][pull["intext"]] += 1
 
-get_all_repos()
+def main(argv):
+    parser = argparse.ArgumentParser(description="Summarize pull requests.")
+    parser.add_argument("--monthly", action="store_true",
+        help="Report on months instead of quarters"
+    )
+    args = parser.parse_args(argv[1:])
+
+    if args.monthly:
+        date_bucket_fn = date_bucket_month
+    else:
+        date_bucket_fn = date_bucket_quarter
+
+    get_all_repos(date_bucket_fn)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
