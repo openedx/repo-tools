2b6257609608d202974e818545d5d33af04d51ff
Ned Batchelder <ned@nedbatchelder.com>
2014-10-22 15:10:51 -0400

    Adapt this to the new people.yaml format

--- non-edx-people.py
+++ non-edx-people.py
@@ -7,5 +7,5 @@ with open("people.yaml") as people_yaml:
    people = yaml.load(people_yaml)
 non_edx = (e for e in people.itervalues() if e.get('institution') != 'edX')
 email_ok = (e for e in non_edx if e.get('email_ok', True))
-emails = (e['authors_entry'].partition('<')[2].strip(">") for e in email_ok)
-print ",\n".join(em for em in emails if em.strip())
+emails = (e.get('email', '').strip() for e in email_ok)
+print ",\n".join(em for em in emails if em)
