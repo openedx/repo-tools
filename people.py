3874bb18ba19246e3757be88baec873afa14cb32
Ned Batchelder <ned@nedbatchelder.com>
2015-01-23 17:22:03 -0500

    Refactor people.py to have a generic yamldata thing

--- people.py
+++ people.py
@@ -1,49 +1,16 @@
 """Access to the people.yaml database."""
 
-import os
-
-import yaml
-
-from helpers import requests
+import yamldata
 
 
-class People(object):
+class People(yamldata.YamlData):
     """
     A database of people.
     """
-    the_people = None
-
-    def __init__(self, data):
-        self.data = data
-
-    @classmethod
-    def from_file(cls, f):
-        """Returns a People object loaded from a yaml file."""
-        return cls(yaml.safe_load(f))
-
-    @classmethod
-    def from_string(cls, s):
-        """Returns a People object loaded from a yaml string."""
-        return cls(yaml.safe_load(s))
 
     @classmethod
     def people(cls):
-        """
-        Returns the main people database.
-        """
-        if cls.the_people is None:
-            # Define REPO_TOOLS_LATEST_PEOPLE=1 in the environment to force code to
-            # get people.yaml from GitHub instead of the local copy.
-            if int(os.environ.get('REPO_TOOLS_LATEST_PEOPLE', '0')):
-                # Read people.yaml from GitHub.
-                people_resp = requests.get("https://raw.githubusercontent.com/edx/repo-tools/master/people.yaml")
-                if not people_resp.ok:
-                    people_resp.raise_for_status()
-                cls.the_people = cls.from_string(people_resp.text)
-            else:
-                with open("people.yaml") as fpeople:
-                    cls.the_people = cls.from_file(fpeople)
-        return cls.the_people
+        return cls.the_data("people.yaml")
 
     def get(self, who, when=None):
         """
---
+++ yamldata.py
@@ -0,0 +1,48 @@
+"""Convenient access to data stored in yaml files."""
+
+import os
+
+import yaml
+
+from helpers import requests
+
+
+class YamlData(object):
+    """Data stored in yaml, conveniently"""
+
+    # A dict mapping filenames to the data we read from the file.
+    _the_data = {}
+
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def from_file(cls, f):
+        """Returns a YamlData object loaded from an open yaml file."""
+        return cls(yaml.safe_load(f))
+
+    @classmethod
+    def from_string(cls, s):
+        """Returns a YamlData object loaded from a yaml string."""
+        return cls(yaml.safe_load(s))
+
+    @classmethod
+    def the_data(cls, filename):
+        """
+        Returns the data from a particular file name, either locally or remote.
+        """
+        if filename not in cls._the_data:
+            # Define REPO_TOOLS_LATEST_PEOPLE=1 in the environment to force code to
+            # get the data from GitHub instead of the local copy.
+            if int(os.environ.get('REPO_TOOLS_LATEST_PEOPLE', '0')):
+                # Read from GitHub.
+                resp = requests.get("https://raw.githubusercontent.com/edx/repo-tools/master/" + filename)
+                if not resp.ok:
+                    resp.raise_for_status()
+                cls._the_data[filename] = cls.from_string(resp.text)
+            else:
+                # Read from a file.
+                with open(filename) as f:
+                    cls._the_data[filename] = cls.from_file(f)
+
+        return cls._the_data[filename]
