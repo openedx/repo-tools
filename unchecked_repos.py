5ae185dbe65e56e82ab620090f852611f26e2435
Ned Batchelder <ned@nedbatchelder.com>
2015-06-17 14:16:32 -0400

    unchecked_repos.py can tell us about repos that are gone, and django-oscar-extensions is gone.

--- unchecked_repos.py
+++ unchecked_repos.py
@@ -12,7 +12,22 @@ REPOS_URL = "https://api.github.com/orgs/{org}/repos"
 with open("repos.yaml") as repos_yaml:
     tracked_repos = yaml.load(repos_yaml)
 
-for r in paginated_get(REPOS_URL.format(org="edX")):
+repos = list(paginated_get(REPOS_URL.format(org="edX")))
+
+shown_any = False
+for r in repos:
     if not r['private'] and not r['fork']:
         if r['full_name'] not in tracked_repos:
+            if not shown_any:
+                print("\n### Untracked repos:")
             print("{r[full_name]}: {r[description]}".format(r=r))
+            shown_any = True
+
+shown_any = False
+actual_repos = set(r['full_name'] for r in repos)
+for tracked in tracked_repos:
+    if tracked not in actual_repos:
+        if not shown_any:
+            print("\n### Disappeared repos:")
+        print(tracked)
+        shown_any = True
