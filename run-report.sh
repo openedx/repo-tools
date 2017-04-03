d47e38ad50928b8e8310b7927b026251ab2fdd39
Joel Barciauskas <joel@edx.org>
2017-04-03 10:10:52 -0400

    Add date constraint to run report shell script

--- run-report.sh
+++ run-report.sh
@@ -2,7 +2,7 @@ cd ../repo-tools-data
 git pull
 cd ../repo-tools
 source venv/bin/activate
-python monthly_pr_stats.py
+python monthly_pr_stats.py --start=6/1/2015
 python pull_quarters.py --monthly --start=6/1/2015
 python transitions_kpi.py --all --since=30
 python pull_orgs.py --since=31 --short
