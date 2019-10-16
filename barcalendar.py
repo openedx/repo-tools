2791d75c3f45fb15f6e7c5017c6a1b22c5d7fc8c
Ned Batchelder <ned@edx.org>
2019-10-16 07:55:06 -0400

    Better implementation

--- barcalendar.py
+++ barcalendar.py
@@ -15,43 +15,49 @@ import datetime
 import itertools
 import sys
 
-# Options
-START_YEAR = 2016
-END_YEAR = 2024
-LTS_ONLY = True
-
-
 EXTEND = '-'
 
-class Calendar:
+class BaseCalendar:
     def __init__(self, start, end):
         self.start = start
-        self.blanks = [''] * 12 * (end - start + 1)
+        self.end = end
+        self.width = 12 * (end - start + 1)
+
+    def column(self, year, month):
+        return (year - self.start) * 12 + month - 1
+
+    def bar(self, name, start, end=None, length=None, color=None, text_color="black"):
+        istart = self.column(*start)
+        if length is None:
+            iend = self.column(*end)
+        else:
+            iend = istart + length - 1
+        if istart >= self.width:
+            return  # bar is entirely in the future.
+        if iend < 0:
+            return  # bar is entirely in the past.
+        istart = max(0, istart)
+        iend = min(self.width - 1, iend)
+        self.rawbar(istart, iend, name, color, text_color)
+
+
+class CsvCalendar(BaseCalendar):
+    def __init__(self, start, end):
+        super().__init__(start, end)
         self.rows = []
+
+    def years_months(self):
         years = []
         months = []
-        for year in range(start, end+1):
+        for year in range(self.start, self.end+1):
             years.append(year)
             years.extend([EXTEND]*11)
             months.extend("JFMAMJJASOND")
         self.rows.append(years)
         self.rows.append(months)
 
-    def bar(self, name, start, end=None, length=None, color=None):
-        row = list(self.blanks)
-        year, month = start
-        istart = (year - self.start) * 12 + month - 1
-        if length is None:
-            eyear, emonth = end
-            iend = (eyear - self.start) * 12 + emonth - 1
-        else:
-            iend = istart + length - 1
-        if istart >= len(row):
-            return  # bar is entirely in the future.
-        if iend < 0:
-            return  # bar is entirely in the past.
-        istart = max(0, istart)
-        iend = min(len(row)-1, iend)
+    def rawbar(self, istart, iend, name, color, text_color):
+        row = [''] * self.width
         if color is not None:
             name += "|" + color
         row[istart] = name
@@ -59,14 +65,152 @@ class Calendar:
             row[ii] = EXTEND
         self.rows.append(row)
 
-    def write(self, outfile):
-        writer = csv.writer(outfile)
+    def write(self):
+        writer = csv.writer(sys.stdout)
         for row in self.rows:
             writer.writerow(row)
 
-cal = Calendar(START_YEAR, END_YEAR)
+
+class NewCalendar(BaseCalendar):
+    def __init__(self, start, end):
+        super().__init__(start, end)
+        self.currow = 1
+        self.cycling = None
+        self.prologue()
+
+    def prologue(self):
+        print(f"""\
+            function makeBarCalendar() {{
+            var sheet = SpreadsheetApp.getActiveSheet();
+            sheet.getDataRange().deleteCells(SpreadsheetApp.Dimension.ROWS);
+            """)
+
+    def epilog(self):
+        print(f"""\
+            range = sheet.getDataRange();
+            sheet.setColumnWidths(range.getColumn(), range.getWidth(), 12);
+            sheet.setRowHeights(range.getRow(), range.getHeight(), 18);
+            range.setFontSize(9);
+
+            var keepRows = 10;
+            var tooMany = sheet.getMaxRows() - range.getLastRow() - keepRows;
+            if (tooMany > 0) {{
+                sheet.deleteRows(range.getLastRow() + keepRows + 1, tooMany);
+            }}
+            var keepCols = 1;
+            var tooMany = sheet.getMaxColumns() - range.getLastColumn() - keepCols;
+            if (tooMany > 0) {{
+                sheet.deleteColumns(range.getLastColumn() + keepCols + 1, tooMany);
+            }}
+
+            for (var c = 12; c <= range.getLastColumn(); c += 12) {{
+                sheet.getRange(1, c, sheet.getMaxRows(), 1)
+                    .setBorder(null, null, null, true, null, null, "black", null);
+            }}
+
+            }}
+            """)
+
+    def years_months(self):
+        yearrow = self.currow
+        monthrow = self.currow + 1
+        self.currow += 2
+
+        print(f"""\
+            sheet.insertColumns(1, {(self.end - self.start + 1) * 12});
+            """)
+
+        for year in range(self.start, self.end+1):
+            iyear = self.column(year, 1) + 1
+            print(f"""\
+                sheet.getRange({yearrow}, {iyear}, 1, 12)
+                    .merge()
+                    .setBorder(null, null, null, true, null, null, "black", null)
+                    .setValue("{year}");
+                for (m = 0; m < 12; m++) {{
+                    sheet.getRange({monthrow}, {iyear}+m).setValue("JFMAMJJASOND"[m]);
+                }}
+                """);
+        print(f"""\
+            sheet.getRange({yearrow}, 1, 1, {self.width})
+                .setFontWeight("bold")
+                .setHorizontalAlignment("center");
+            sheet.getRange({monthrow}, 1, 1, {self.width})
+                .setHorizontalAlignment("center");
+            """);
+        print(f"""\
+            var rules = sheet.getConditionalFormatRules();
+            rules.push(
+            SpreadsheetApp.newConditionalFormatRule()
+                .whenFormulaSatisfied("=(year(now())-$A$1)*12+1 = column()")
+                .setBackground("#E9CECE")
+                .setRanges([sheet.getRange({yearrow}, 1, 1, {self.width})])
+                .build()
+            );
+            rules.push(
+            SpreadsheetApp.newConditionalFormatRule()
+                .whenFormulaSatisfied("=(year(now())-$A$1)*12 + (month(now())) = column()")
+                .setBackground("#E9CECE")
+                .setRanges([sheet.getRange({monthrow}, 1, 1, {self.width})])
+                .build()
+            );
+            sheet.setConditionalFormatRules(rules);
+            """)
+
+    def rawbar(self, istart, iend, name, color, text_color):
+        print(f"""\
+            sheet.getRange({self.currow}, {istart + 1}, 1, {iend - istart + 1})
+                .merge()
+                .setBackground({color!r})
+                .setFontColor({text_color!r})
+                .setValue({name!r});
+            """)
+        self.next_bar()
+
+    def set_cycling(self, cycling):
+        if cycling:
+            self.top_cycling_row = self.currow
+        else:
+            self.currow = self.top_cycling_row + self.cycling
+        self.cycling = cycling
+
+    def next_bar(self):
+        self.currow += 1
+        if self.cycling:
+            if self.currow >= self.top_cycling_row + self.cycling:
+                self.currow = self.top_cycling_row
+
+    def text_line(self, text=""):
+        print(f"""\
+            sheet.getRange({self.currow}, 1).setValue({text!r})
+            """)
+        self.currow += 1
+
+    def freeze_here(self):
+        print(f"""\
+            sheet.setFrozenRows({self.currow - 1});
+            """)
+
+    def write(self):
+        self.epilog()
+
+
+# Options
+START_YEAR = 2016
+END_YEAR = 2024
+LTS_ONLY = True
+
+
+
+if sys.argv[1] == "new":
+    cal = NewCalendar(START_YEAR, END_YEAR)
+else:
+    cal = CsvCalendar(START_YEAR, END_YEAR)
+cal.years_months()
+
 
 # Open edX releases
+cal.set_cycling(3)
 names = [
     ('Aspen', 2014, 10),
     ('Birch', 2015, 2),
@@ -94,6 +238,10 @@ for (name, year, month), (_, nextyear, nextmonth) in zip(releases, releases[1:])
         length = (nextyear * 12 + nextmonth) - (year * 12 + month)
     cal.bar(name, start=(year, month), length=length, color="#fce5cd")
     last = (year, month)
+
+cal.set_cycling(None)
+cal.freeze_here()
+cal.text_line("(this calendar is part of OEP-10, please don't change it without considering the impact there.)")
 
 # Django releases
 django_releases = [
@@ -136,7 +284,7 @@ for year, month in itertools.product(range(16, 23), [4, 10]):
         continue
     length = 5*12 if lts else 9
     color = "#E95420" if lts else "#F4AA90"     # http://design.ubuntu.com/brand/colour-palette
-    cal.bar(name, (2000+year, month), length=length, color=color)
+    cal.bar(name, (2000+year, month), length=length, color=color, text_color="white")
 
 # Node releases: https://github.com/nodejs/Release
 node_releases = [
@@ -147,10 +295,10 @@ node_releases = [
     ('14.x', 2020, 4, 2023, 4),
 ]
 for name, syear, smonth, eyear, emonth in node_releases:
-    cal.bar(f"Node {name}", start=(syear, smonth), end=(eyear, emonth), color="#2f6c1b")
+    cal.bar(f"Node {name}", start=(syear, smonth), end=(eyear, emonth), color="#2f6c1b", text_color="white")
 
 
-cal.write(sys.stdout)
+cal.write()
 
 
 # The code for Google Sheets to turn this output into something nice.
