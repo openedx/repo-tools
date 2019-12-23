e6fd2f0a2685c871c0107301cb390019cb49aef1
Feanil Patel <feanil@edx.org>
2019-12-23 15:27:51 -0500

    Rename python3 tools folder to ownership_tools.
    We used this tooling for python 3 but is generally useful to
    understand the state of all our repos and ownership.

--- ownership_tools/get_all_repos.py
+++ ownership_tools/get_all_repos.py
@@ -2,8 +2,6 @@ from get_repos import expanded_repos_list, orgs, total_language_bytes
 
 all_repos = expanded_repos_list(orgs)
 
-repo_size_map = {}
-
 for repo in all_repos:
     if not repo.archived:
         print(",".join([repo.name, repo.html_url, str(repo.fork), total_language_bytes(repo)]))
