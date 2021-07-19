45649207f96f727aae415ba3ab3a76a4c22297b1
Ned Batchelder <ned@edx.org>
2021-07-19 16:50:50 -0400

    feat!: remove unneeded sync_labels command
    This is now handled continuously by the OSPR bot: https://github.com/edx/openedx-webhooks/blob/4364b99c9d8a3346613eb1e69a9539e7eb793516/openedx_webhooks/tasks/github_work.py#L15-L17

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
             'clone_org = edx_repo_tools.dev.clone_org:main',
             'show_hooks = edx_repo_tools.dev.show_hooks:main',
             'oep2 = edx_repo_tools.oep2:_cli',
-            'sync_labels = edx_repo_tools.ospr.sync_labels:sync_labels',
             'no_yaml = edx_repo_tools.ospr.no_yaml:no_yaml',
             'tag_release = edx_repo_tools.release.tag_release:main',
             'drip = edx_repo_tools.drip_survey:cli',
