3a3ee37b4c9f81107c40088556be90702eff386c
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-01-27 20:45:55 +0500

    feat: intial work with the classifier modernizer, new classifier still to be added

--- setup.py
+++ setup.py
@@ -36,6 +36,11 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License'
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
     ],
     packages=setuptools.find_packages(),
     install_requires=[
