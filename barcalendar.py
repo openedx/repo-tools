184e3ba0c70951d0c074adb038acebb0f49ab695
Ned Batchelder <ned@edx.org>
2022-08-19 14:50:51 -0400

    fix(barcalendar): the current release is Nutmeg

--- barcalendar.py
+++ barcalendar.py
@@ -246,7 +246,7 @@ LTS_ONLY = True
 
 # The current versions of everything.  Use the same strings as the keys in the various sections below.
 CURRENT = {
-    "Open edX": "Maple",
+    "Open edX": "Nutmeg",
     "Python": "3.8",
     "Django": "3.2",
     "Ubuntu": "20.04",
