3aa08a0f6eb0cb487fc8f5cda37ba022cd67b27e
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-05-09 21:34:57 +0500

    fix: add reviewers in for dependabot config

--- edx_repo_tools/dependabot_yml.py
+++ edx_repo_tools/dependabot_yml.py
@@ -11,6 +11,10 @@ github_actions = """\
     schedule:
         interval: "weekly"
 """
+ecosystem_reviewers = """\
+    reviewers:
+      - "{reviewer}"
+"""
 
 # Adding new packages for update. Add tuple with key and related data.
 ADD_NEW_FIELDS = [("github-actions", github_actions,)]
@@ -21,8 +25,9 @@ class DependabotYamlModernizer(YamlLoader):
     Dependabot Yaml Modernizer class is responsible for adding new elements in dependabot.yml.
     """
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, reviewer):
         super().__init__(file_path)
+        self.reviewer = reviewer
 
     def _add_elements(self):
         self.elements['updates'] = self.elements.get('updates') or []
@@ -36,8 +41,22 @@ class DependabotYamlModernizer(YamlLoader):
         if not found:
             self.elements['updates'].append(self.yml_instance.load(value))
 
+
+    def _add_reviewers(self):
+        self.elements['updates'] = self.elements.get('updates') or []
+        for key, value in ADD_NEW_FIELDS:
+            for index, elem in enumerate(self.elements['updates']):
+                if key == elem.get('package-ecosystem'):
+                    if 'reviewers' not in elem:
+                        self.elements["updates"][index].update(self.yml_instance.load(
+                            ecosystem_reviewers.format(**{"reviewer": self.reviewer})
+                        ))
+                    break
+
+
     def modernize(self):
         self._add_elements()
+        self._add_reviewers()
         # otherwise it brings back whole update back towards left side.
 
         self.yml_instance.indent(mapping=4, sequence=4, offset=2)
@@ -48,7 +67,10 @@ class DependabotYamlModernizer(YamlLoader):
 @click.option(
     '--path', default='.github/dependabot.yml',
     help="Path to target dependabot.yml file")
-def main(path):
+@click.option(
+    '--reviewer', default=None,
+    help="Name of the reviewer")
+def main(path, reviewer):
     if not os.path.exists(path):
         new_file_content = """\
             version: 2
@@ -56,7 +78,7 @@ def main(path):
         """
         with open(path, 'w') as file:
             file.write(new_file_content)
-    modernizer = DependabotYamlModernizer(path)
+    modernizer = DependabotYamlModernizer(path, reviewer)
     modernizer.modernize()
 
 
