cde0cb3cca8320d070a8020849a19eb10fab3119
Jimmy <jimmy.b.mau@gmail.com>
2023-07-10 14:42:31 -0400

    fix: Update Elasticsearch support dates
    Add support date for Elasticsearch version 7.17.
    
    Did not include support dates for previous versions between 7.14 and
    7.16 inclusive.

--- barcalendar.py
+++ barcalendar.py
@@ -440,11 +440,12 @@ es_releases = [
     # ('2.4', 2016, 8, 2018, 2),
     # ('5.6', 2017, 9, 2019, 3),
     # ('6.8', 2019, 5, 2020, 11),
-    ('7.8', 2020, 6, 2021, 12),
+    # ('7.8', 2020, 6, 2021, 12),
     ('7.10', 2020, 11, 2022, 5),
     ('7.11', 2021, 2, 2022, 8),
     ('7.12', 2021, 3, 2022, 9),
     ('7.13', 2021, 5, 2022, 11),
+    ('7.17', 2022, 2, 2023, 8),
 ]
 for name, syear, smonth, eyear, emonth in es_releases:
     cal.bar(f"Elasticsearch {name}", start=(syear, smonth), end=(eyear, emonth), color="#4595ba", current=(name==CURRENT["Elasticsearch"]))
