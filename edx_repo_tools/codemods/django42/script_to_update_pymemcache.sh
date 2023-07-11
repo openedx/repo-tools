f8ba8cd90e08ab87c156d5763da6d903c481dae3
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-07-11 16:06:28 +0500

    fix: requirements removal reverted

--- edx_repo_tools/codemods/django42/script_to_update_pymemcache.sh
+++ edx_repo_tools/codemods/django42/script_to_update_pymemcache.sh
@@ -6,18 +6,6 @@ function update_cache_backend() {
     find "$root_dir" -type f -exec perl -i -pe 's/django\.core\.cache\.backends\.memcached\.MemcachedCache/django.core.cache.backends.memcached.PyMemcacheCache/g' {} +
 }
 
-# Function to find and append "pymemcache" to base.in files
-function append_pymemcache() {
-    local root_dir=$1
-    find "$root_dir" -type f -name "base.in" -exec sh -c 'echo "pymemcache" >> "$0"' {} \;
-}
-
-# Function to find and remove the line containing "python-memcached==<version>"
-function remove_python_memcached() {
-    local root_dir=$1
-    find "$root_dir" -type f -exec perl -i -ne 'print unless /^python-memcached==.*$/' {} +
-}
-
 # Get the current directory
 current_dir=$(pwd)
 
@@ -25,13 +13,3 @@ current_dir=$(pwd)
 echo "Updating cache backend in project: $current_dir"
 update_cache_backend "$current_dir"
 echo "Cache backend update completed in project: $current_dir"
-
-# Append "pymemcache" to base.in files
-echo "Appending 'pymemcache' to base.in files in project: $current_dir"
-append_pymemcache "$current_dir"
-echo "Appending 'pymemcache' completed in project: $current_dir"
-
-# Remove lines containing "python-memcached==<version>"
-echo "Removing 'python-memcached' lines in project: $current_dir"
-remove_python_memcached "$current_dir"
-echo "Removal of 'python-memcached' lines completed in project: $current_dir"
