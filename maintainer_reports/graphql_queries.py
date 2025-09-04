a38815b725dd469b18d6c6df8849785ca0414670
Ty Hob <bmesick@tcril.org>
2025-09-04 13:02:00 -0400

    refactor: Remove old repos from find_dependencies
    Removes Insights and Ecommerce repos, but this is not a full overhaul, I did not do any other
    research on which other repos should be added or removed.

--- maintainer_reports/graphql_queries.py
+++ maintainer_reports/graphql_queries.py
@@ -1,10 +1,10 @@
-_REPOS_ = "repo:openedx/DoneXBlock repo:openedx/RateXBlock repo:openedx/XBlock repo:openedx/blockstore repo:openedx/brand-openedx repo:openedx/codejail repo:openedx/configuration repo:openedx/course-discovery repo:openedx/credentials repo:openedx/cs_comments_service repo:openedx/devstack repo:openedx/docs.openedx.org repo:openedx/ecommerce repo:openedx/ecommerce-worker repo:openedx/edx-ace repo:openedx/edx-analytics-dashboard repo:openedx/edx-analytics-data-api repo:openedx/edx-analytics-pipeline repo:openedx/edx-app-android repo:openedx/edx-cookiecutters repo:openedx/edx-developer-docs repo:openedx/edx-documentation repo:openedx/edx-notes-api repo:openedx/edx-ora2 repo:openedx/edx-platform repo:openedx/edx-proctoring repo:openedx/edx-rest-api-client repo:openedx/edx-search repo:openedx/edx-toggles repo:openedx/enterprise-catalog repo:openedx/event-routing-backends repo:openedx/frontend-app-account repo:openedx/frontend-app-admin-portal repo:openedx/frontend-app-authn repo:openedx/frontend-app-course-authoring repo:openedx/frontend-app-discussions repo:openedx/frontend-app-ecommerce repo:openedx/frontend-app-gradebook repo:openedx/frontend-app-learning repo:openedx/frontend-app-payment repo:openedx/frontend-app-profile repo:openedx/frontend-app-program-console repo:openedx/frontend-app-publisher repo:openedx/frontend-app-support-tools repo:openedx/frontend-build repo:openedx/frontend-component-footer repo:openedx/frontend-component-header repo:openedx/frontend-platform repo:openedx/frontend-template-application repo:openedx/license-manager repo:openedx/mdrst repo:openedx/open-edx-proposals repo:openedx/openedx-conference-website repo:openedx/openedx-demo-course repo:openedx/openedx-events repo:openedx/openedx-filters repo:openedx/openedx-i18n repo:openedx/paragon repo:openedx/taxonomy-connector repo:openedx/xblock-drag-and-drop-v2 repo:openedx/xblock-lti-consumer repo:openedx/xqueue repo:openedx/frontend-app-library-authoring repo:openedx/frontend-app-learner-record repo:openedx/frontend-app-authn  repo:openedx/django-config-models repo:openedx/edx-bulk-grades repo:openedx/edx-django-utils repo:openedx/edx-val repo:openedx/frontend-app-discussions  repo:openedx/frontend-app-ora-grading repo:openedx/xblock-sdk repo:openedx/xblock-utils"
+_REPOS_ = "repo:openedx/DoneXBlock repo:openedx/RateXBlock repo:openedx/XBlock repo:openedx/blockstore repo:openedx/brand-openedx repo:openedx/codejail repo:openedx/configuration repo:openedx/course-discovery repo:openedx/credentials repo:openedx/cs_comments_service repo:openedx/devstack repo:openedx/docs.openedx.org repo:openedx/edx-ace repo:openedx/edx-app-android repo:openedx/edx-cookiecutters repo:openedx/edx-developer-docs repo:openedx/edx-documentation repo:openedx/edx-notes-api repo:openedx/edx-ora2 repo:openedx/edx-platform repo:openedx/edx-proctoring repo:openedx/edx-rest-api-client repo:openedx/edx-search repo:openedx/edx-toggles repo:openedx/enterprise-catalog repo:openedx/event-routing-backends repo:openedx/frontend-app-account repo:openedx/frontend-app-admin-portal repo:openedx/frontend-app-authn repo:openedx/frontend-app-course-authoring repo:openedx/frontend-app-discussions repo:openedx/frontend-app-ecommerce repo:openedx/frontend-app-gradebook repo:openedx/frontend-app-learning repo:openedx/frontend-app-payment repo:openedx/frontend-app-profile repo:openedx/frontend-app-program-console repo:openedx/frontend-app-publisher repo:openedx/frontend-app-support-tools repo:openedx/frontend-build repo:openedx/frontend-component-footer repo:openedx/frontend-component-header repo:openedx/frontend-platform repo:openedx/frontend-template-application repo:openedx/license-manager repo:openedx/mdrst repo:openedx/open-edx-proposals repo:openedx/openedx-conference-website repo:openedx/openedx-demo-course repo:openedx/openedx-events repo:openedx/openedx-filters repo:openedx/openedx-i18n repo:openedx/paragon repo:openedx/taxonomy-connector repo:openedx/xblock-drag-and-drop-v2 repo:openedx/xblock-lti-consumer repo:openedx/xqueue repo:openedx/frontend-app-library-authoring repo:openedx/frontend-app-learner-record repo:openedx/frontend-app-authn  repo:openedx/django-config-models repo:openedx/edx-bulk-grades repo:openedx/edx-django-utils repo:openedx/edx-val repo:openedx/frontend-app-discussions  repo:openedx/frontend-app-ora-grading repo:openedx/xblock-sdk repo:openedx/xblock-utils"
 
-_TYPE_="pr"
-_STATE_="closed"
-_QUERY_= f"{_REPOS_} is:{_TYPE_} is:{_STATE_} closed:_RANGE_"
+_TYPE_ = "pr"
+_STATE_ = "closed"
+_QUERY_ = f"{_REPOS_} is:{_TYPE_} is:{_STATE_} closed:_RANGE_"
 
-CLOSED_ISSUE_QUERY="""
+CLOSED_ISSUE_QUERY = """
 {
   search(first: 75, after: _END_CURSOR_, query: "_QUERY_", type: ISSUE) {
     pageInfo {
@@ -25,7 +25,7 @@ CLOSED_ISSUE_QUERY="""
           login
         }
         mergedBy {
-         login 
+         login
         }
         permalink
         createdAt
@@ -57,15 +57,14 @@ CLOSED_ISSUE_QUERY="""
     }
   }
 }
-""".replace("_QUERY_",_QUERY_)
+""".replace("_QUERY_", _QUERY_)
 
 
+_TYPE_ = "pr"
+_STATE_ = "open"
+_OPEN_QUERY_ = f"{_REPOS_} is:{_TYPE_} is:{_STATE_}"
 
-_TYPE_="pr"
-_STATE_="open"
-_OPEN_QUERY_= f"{_REPOS_} is:{_TYPE_} is:{_STATE_}"
-
-OPEN_ISSUE_QUERY="""
+OPEN_ISSUE_QUERY = """
 {
   search(first: 75, after: _END_CURSOR_, query: "_OPEN_QUERY_", type: ISSUE) {
     pageInfo {
@@ -86,7 +85,7 @@ OPEN_ISSUE_QUERY="""
           login
         }
         mergedBy {
-         login 
+         login
         }
         permalink
         createdAt
@@ -118,7 +117,7 @@ OPEN_ISSUE_QUERY="""
     }
   }
 }
-""".replace("_OPEN_QUERY_",_OPEN_QUERY_)
+""".replace("_OPEN_QUERY_", _OPEN_QUERY_)
 
 
 ISSUES_ONLY_QUERY = """
@@ -170,7 +169,7 @@ REPOSITORY_QUERY = """
           startCursor
           hasNextPage
           endCursor
-      } 
+      }
       nodes {
         ... on Repository {
           name
@@ -222,4 +221,3 @@ REPOSITORY_QUERY = """
     }
   }
 """
-
